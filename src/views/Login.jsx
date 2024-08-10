fc786a9cdd38cb530074a33a96c720c7df9ee78c
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-08-10 15:05:49 +0700

    change api

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
           setUserData(reply.data, reply.token);
           axios({
             method: 'GET',
-            url: 'https://siperatdata.azurewebsites.net/is_admin',
+            url: 'https://siperat-33o7oo4n.b4a.run/is_admin',
             headers: {
               Authorization: `Bearer ${getToken}`,
             },
